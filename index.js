5a86b26ea05b8275c2ff37c5af5e6472972db415
AmirHPartovi <a.partovi99@gmail.com>
2023-04-20 16:34:28 +0330

    routing

--- index.js
+++ index.js
@@ -5,23 +5,40 @@ const PORT = 3000
 const friends =[
     {
         id : 0 ,
-        name : 'isaac newton'
+        name : 'isaac newton',
     },
     {
         id : 1 ,
-        name : 'cristiano ronaldo'
+        name : 'cristiano ronaldo',
     },
     {
         id : 2 ,
-        name : 'leonardo dicaprio'
+        name : 'leonardo dicaprio',
     },
 ]
 
 server.on('request',(req,res)=>{
-    res.writeHead(200,{
-        'Content-Type':'application/json',
-    });
-    res.end(JSON.stringify(friends))
+    if(req.url === '/friends'){
+        
+        // res.writeHead(200,{
+        //     'Content-Type':'application/json',
+        // });
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'application/json');
+        res.end(JSON.stringify(friends));
+    }else if (req.url === '/messages'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li> hello amir</li>');
+        res.write('<li>this is some dummy text ...</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end()
+    }
 })
 
 
